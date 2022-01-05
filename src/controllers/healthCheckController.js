f58836d552dfeab7503da4439e2740efab31f704
Rick Brown <ricky.brown.00@gmail.com>
2022-01-05 16:35:00 +0000

    updates health check controller

--- src/controllers/healthCheckController.js
+++ src/controllers/healthCheckController.js
@@ -2,11 +2,13 @@ const config = require('../config/config')
 const logger = require('../middleware/logging')
 const NAMESPACE = 'HEALTH CHECK CONTROLLER'
 
-const healthCheckController = (req, res, next) => {
+const healthCheckController = (req, res) => {
 	logger.info(NAMESPACE, `Server health check called.`)
 
 	return res.status(200).json({
-		message: `✅ : server running on ${config.server.name}:${config.server.port}`
+		status: 200,
+		endpoint: `${config.server.url}/health/check`,
+		message: `server successfully running on ${config.server.name}:${config.server.port}.`
 	})
 }
 
