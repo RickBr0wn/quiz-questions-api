1c2e54129dd14ebcbf94edfa7a0a14e57e3c9036
Rick Brown <ricky.brown.00@gmail.com>
2022-01-05 16:34:45 +0000

    updates config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,13 @@ dotenv.config()
 
 const SERVER_NAME = process.env.HOSTNAME || 'localhost'
 const SERVER_PORT = process.env.PORT || 1337
+const BASE_URL =
+	process.env.NODE_ENV === 'production'
+		? 'https://warm-island-43015.herokuapp.com'
+		: 'localhost:1337'
 
 const SERVER = {
+	url: BASE_URL,
 	name: SERVER_NAME,
 	port: SERVER_PORT
 }
