a181b95247a86e43a2f77a8dc10dacf9c734dd4d
Rick Brown <ricky.brown.00@gmail.com>
2022-01-07 14:57:33 +0000

    updates logger for heroku log --tail

--- src/middleware/logging.js
+++ src/middleware/logging.js
@@ -2,42 +2,74 @@ const getTimeStamp = () =>
 	new Date().toLocaleDateString() + ' - ' + new Date().toLocaleTimeString()
 
 const error = (namespace, message, object) => {
-	if (object) {
-		console.error(
-			`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}, ${object}`
-		)
+	if (process.env.NODE_ENV === 'production') {
+		if (object) {
+			console.error(`[ERROR] [${namespace}] ${message}, ${object}`)
+		} else {
+			console.error(`[ERROR] [${namespace}] ${message}`)
+		}
 	} else {
-		console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`)
+		if (object) {
+			console.error(
+				`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}, ${object}`
+			)
+		} else {
+			console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`)
+		}
 	}
 }
 
 const debug = (namespace, message, object) => {
-	if (object) {
-		console.debug(
-			`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}, ${object}`
-		)
+	if (process.env.NODE_ENV === 'production') {
+		if (object) {
+			console.debug(`[DEBUG] [${namespace}] ${message}, ${object}`)
+		} else {
+			console.debug(`[DEBUG] [${namespace}] ${message}`)
+		}
 	} else {
-		console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`)
+		if (object) {
+			console.debug(
+				`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}, ${object}`
+			)
+		} else {
+			console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`)
+		}
 	}
 }
 
 const info = (namespace, message, object) => {
-	if (object) {
-		console.info(
-			`[${getTimeStamp()}] [INFO] [${namespace}] ${message}, ${object}`
-		)
+	if (process.env.NODE_ENV === 'production') {
+		if (object) {
+			console.info(`[INFO] [${namespace}] ${message}, ${object}`)
+		} else {
+			console.info(`[INFO] [${namespace}] ${message}`)
+		}
 	} else {
-		console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`)
+		if (object) {
+			console.info(
+				`[${getTimeStamp()}] [INFO] [${namespace}] ${message}, ${object}`
+			)
+		} else {
+			console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`)
+		}
 	}
 }
 
 const warn = (namespace, message, object) => {
-	if (object) {
-		console.warn(
-			`[${getTimeStamp()}] [WARN] [${namespace}] ${message}, ${object}`
-		)
+	if (process.env.NODE_ENV === 'production') {
+		if (object) {
+			console.warn(`[WARN] [${namespace}] ${message}, ${object}`)
+		} else {
+			console.warn(`[WARN] [${namespace}] ${message}`)
+		}
 	} else {
-		console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`)
+		if (object) {
+			console.warn(
+				`[${getTimeStamp()}] [WARN] [${namespace}] ${message}, ${object}`
+			)
+		} else {
+			console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`)
+		}
 	}
 }
 
